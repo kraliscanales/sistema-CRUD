0ab95eb032dd162a0fa69786ddb508828e7206f9
Kralis <kraliscanales89@gmail.com>
2026-05-18 18:49:02 -0600

    Modelos y migraciones iniciales

--- backend/inventario/inventario/settings.py
+++ backend/inventario/inventario/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'produto',
+    'producto',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'inventario.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+      #  'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inventario',
+        'USER': 'postgres',
+        'PASSWORD': 'Ludyobando0805',
+        'HOST': 'localhost',
+        'PORT': '5432', 
+        }
 }
 
 
--- backend/inventario/producto/models.py
+++ backend/inventario/producto/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.FloatField()
+    descripcion = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
