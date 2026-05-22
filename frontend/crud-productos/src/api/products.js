c16133b9ec5143ed40605af2ae87d751cfcea31e
Kralis <kraliscanales89@gmail.com>
2026-05-22 01:02:46 -0600

    corrigiendo slash

--- frontend/crud-productos/src/api/products.js
+++ frontend/crud-productos/src/api/products.js
@@ -5,7 +5,7 @@ const productsApi = axios.create({
 })
 
 export const getProducts = () => productsApi.get()
-export const getProduct = (id) => productsApi.get(`${id}/`)
+export const getProduct = (id) => productsApi.get(`/${id}/`)
 export const createProduct = (product) => productsApi.post("", product)
-export const updateProduct = (id, product) => productsApi.put(`${id}/`, product)
-export const deleteProduct = (id) => productsApi.delete(`${id}/`)
+export const updateProduct = (id, product) => productsApi.put(`/${id}/`, product)
+export const deleteProduct = (id) => productsApi.delete(`/${id}/`)
