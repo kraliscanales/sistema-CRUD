ce828d67c0d2cddcd91ce81989350ee0343b5fd5
Kralis <kraliscanales89@gmail.com>
2026-05-21 21:56:58 -0600

    Cambiar PostgreSQL a SQLite y ajustar ALLOWED_HOSTS para desarrollo local

--- backend/inventario/inventario/settings.py
+++ backend/inventario/inventario/settings.py
@@ -78,18 +78,11 @@ WSGI_APPLICATION = 'inventario.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-      #  'ENGINE': 'django.db.backends.sqlite3',
-       # 'NAME': BASE_DIR / 'db.sqlite3',
-    #}
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'inventario',
-        'USER': 'postgres',
-        'PASSWORD': 'Ludyobando0805',
-        'HOST': 'localhost',
-        'PORT': '5432', 
-        }
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    
 }
 
 
