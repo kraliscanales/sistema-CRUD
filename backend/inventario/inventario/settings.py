ee5de54b116af2d03ea80c50b95ceaa7526fc2ec
Kralis <kraliscanales89@gmail.com>
2026-05-22 01:29:31 -0600

    configuracion en el settings

--- backend/inventario/inventario/settings.py
+++ backend/inventario/inventario/settings.py
@@ -44,10 +44,11 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -125,5 +126,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = [ 
+        
+       "https://sistema-crud-ub8z-qfi7fol81.vercel.app",
+]
   
