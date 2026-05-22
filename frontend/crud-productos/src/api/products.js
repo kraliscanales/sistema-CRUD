f72cd0e39437311b987f6de091f2213c097eba0b
Kralis <kraliscanales89@gmail.com>
2026-05-21 23:43:14 -0600

    cambiando baseURL

--- frontend/crud-productos/src/api/products.js
+++ frontend/crud-productos/src/api/products.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const productsApi = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/producto/"
+    baseURL: "https://kralis3.pythonanywhere.com/api/producto/"
 })
 
-export const getProducts =  () => productsApi.get()
+export const getProducts = () => productsApi.get()
 export const getProduct = (id) => productsApi.get(`${id}/`)
-export const createProduct = (product) => productsApi.post("/", product)
-export const updateProduct = (id, product) => productsApi.put(`/${id}/`, product)
-export const deleteProduct = (id) => productsApi.delete(`/${id}/`)
+export const createProduct = (product) => productsApi.post("", product)
+export const updateProduct = (id, product) => productsApi.put(`${id}/`, product)
+export const deleteProduct = (id) => productsApi.delete(`${id}/`)
