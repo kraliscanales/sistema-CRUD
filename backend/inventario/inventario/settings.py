3bb88788e448d0c53c77d6d05d7b270baed391d5
Kralis <kraliscanales89@gmail.com>
2026-05-22 00:57:58 -0600

    fix: configurar CORS y URLs HTTPS para producción (Vercel + PythonAnywhere)

--- backend/inventario/inventario/settings.py
+++ backend/inventario/inventario/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$t@)-n+5+lkra88-olt(gh*d%h^3u5228!=2tn5*ohzvkmhjs&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['Kralis3.pythonanywhere.com']
 
@@ -127,6 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "sistema-crud-ub8z-qfi7fol81.vercel.app"
+    "https://sistema-crud-ub8z-qfi7fol81.vercel.app/"
 ]
   
