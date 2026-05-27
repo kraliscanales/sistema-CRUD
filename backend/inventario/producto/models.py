621a4fe2bfbd7f483fd58321569ef25e6bc42bb7
Kralis <kraliscanales89@gmail.com>
2026-05-27 08:54:47 -0600

    Validar nombres de productos sin importar mayúsculas

--- backend/inventario/producto/models.py
+++ backend/inventario/producto/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from django.db.models.functions import Lower
 
 # Create your models here.
 class Producto(models.Model):
-    nombre = models.CharField(max_length=100, unique=True)
+    nombre = models.CharField(max_length=100)
     precio = models.FloatField()
     descripcion = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                Lower('nombre'),
+                name='unique_nombre_insensible'
+            )
+        ]
     
 
     def __str__(self):
